dbf3cf2d7b2749d4aef09823b6d0c30bc6d78ca3
Norman Roa <normanroa97@gmail.com>
2023-03-16 16:56:42 -0600

    Añadido menu principal

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ var boton = document.getElementById('boton');
 boton.addEventListener('click', function() {
   alert('Hola desde mi sitio web');
 });
+
+window.onload = function(){
+	var menuItems = document.querySelectorAll('nav ul li');
+	for(var i=0; i<menuItems.length; i++){
+		if(menuItems[i].querySelector('ul')){
+			menuItems[i].querySelector('a').addEventListener('click', function(e){
+				e.preventDefault();
+				var subMenu = this.parentNode.querySelector('ul');
+				if(subMenu.style.display === 'block'){
+					subMenu.style.display = 'none';
+				} else {
+					subMenu.style.display = 'block';
+				}
+			});
+		}
+		
+	}
+}
